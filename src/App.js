c9c1a9812281d26e998c30521c41420090ea5c79
erikefernandes40 <erikefernandes40@gmail.com>
2022-03-31 21:45:23 -0300

    remocao de arquivos padroes do react e inicio do desenvolvimento do formulario

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import {Formik, Form, Field, ErrorMessage} from "formik"
+import * as yup from "yup"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const handleClickLogin = (values) => console.log(values)
+
+  const validationLogin = yup.object().shape({
+    email: yup.string().email("Insira um email válido").required("Este campo é obrigatório"),
+    password: yup.string("A senha deve conter pelo menos 8 caracteres").min(8).required("Este campo é obrigatótio"),
+  })
+
+  return  <div className='container'>
+    <h1>login</h1>
+    <Formik initialValues={{}} onSubmit={handleClickLogin} validationSchema={validationLogin}>
+      <Form className='login-form'>
+      <div className='login-form-group'>
+          <Field name="email" className="form-field" placeHolder="Email"></Field>
+
+          <ErrorMessage
+          component="span" name="email" className="form-error"
+          />
+
+        </div>
+
+        <div className='login-form-group'>
+          <Field name="password" className="form-field" placeHolder="Senha"></Field>
+
+          <ErrorMessage
+          component="span" name="password" className="form-error"
+          />
+
+        </div>
+
+        <button className='button' type='submit'>Login</button>
+
+      </Form>
+    </Formik>
+
+  </div>
 }
 
 export default App;
