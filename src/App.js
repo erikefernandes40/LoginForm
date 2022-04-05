66e8ee5689d32f052708187f6ab45b2697044ac7
erikefernandes40 <erikefernandes40@gmail.com>
2022-04-05 07:37:05 -0300

    formulario de registro

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import * as yup from "yup"
 
 function App() {
   const handleClickLogin = (values) => console.log(values)
+  const handleClickRegister = (values) => console.log(values)
 
   const validationLogin = yup.object().shape({
     email: yup.string().email("Insira um email válido").required("Este campo é obrigatório"),
     password: yup.string("A senha deve conter pelo menos 8 caracteres").min(8).required("Este campo é obrigatótio"),
   })
 
+  const validationRegister =  yup.object().shape({
+    email: yup.string().email("Insira um email válido").required("Este campo é obrigatório"),
+    password: yup.string("A senha deve conter pelo menos 8 caracteres").min(8).required("Este campo é obrigatótio"),
+    confirmPassword: yup.string().oneOf([yup.ref("password"), null], "As senhas não correspondem"),
+  })
+
   return  <div className='container'>
-    <h1>login</h1>
+    <h1>Login</h1>
     <Formik initialValues={{}} onSubmit={handleClickLogin} validationSchema={validationLogin}>
       <Form className='login-form'>
       <div className='login-form-group'>
@@ -27,7 +34,42 @@ function App() {
           <Field name="password" className="form-field" placeHolder="Senha"></Field>
 
           <ErrorMessage
+          component="span" name="confirmPassword" className="form-error"
+          />
+
+        </div>
+
+        <button className='button' type='submit'>Login</button>
+
+      </Form>
+    </Formik>
+
+    <h1>Cadastro</h1>
+    <Formik initialValues={{}} onSubmit={handleClickRegister} validationSchema={validationRegister}>
+      <Form className='login-form'>
+      <div className='login-form-group'>
+          <Field name="email" className="form-field" placeHolder="Email"></Field>
+
+          <ErrorMessage
+          component="span" name="email" className="form-error"
+          />
+
+        </div>
+
+        <div className='login-form-group'>
+          <Field name="password" className="form-field" placeHolder="Senha"></Field>
+
+          <ErrorMessage
           component="span" name="password" className="form-error"
+          />
+
+        </div>
+
+        <div className='login-form-group'>
+          <Field name="confirmPassword" className="form-field" placeHolder="Confirme sua senha"></Field>
+
+          <ErrorMessage
+          component="span" name="confirmPassword" className="form-error"
           />
 
         </div>
